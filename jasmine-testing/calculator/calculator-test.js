01a2ab4311eebac53de73e6426d842023e06e627
Danny <82071872+tsurugixx@users.noreply.github.com>
2023-06-11 12:58:56 -0500

    wrote some tests for the calc

--- jasmine-testing/calculator/calculator-test.js
+++ jasmine-testing/calculator/calculator-test.js
@@ -1,11 +1,18 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  values = {
+    amount: 12000, years: 5, rate: 13.9
+  }
+  
+  expect(calculateMonthlyPayment(values)).toEqual('278.60');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  values = {
+    amount: 100, years: 5, rate: 7.4
+  }
+  expect(calculateMonthlyPayment(values)).toEqual('2.00');
 });
 
 /// etc
