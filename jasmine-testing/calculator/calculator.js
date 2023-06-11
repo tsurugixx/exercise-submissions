e2dc83e92cc1c15ba19a87c67bfa66e42c813607
Danny <82071872+tsurugixx@users.noreply.github.com>
2023-06-11 12:47:05 -0500

    Coded calculator functions

--- jasmine-testing/calculator/calculator.js
+++ jasmine-testing/calculator/calculator.js
@@ -1,3 +1,6 @@
+
+
+
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
   if (form) {
@@ -21,20 +24,45 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  setInt = {amount: 5000, years:3, rate: 24.99}
+  formAmount = document.getElementById("loan-amount");
+  formYears = document.getElementById("loan-years");
+  formRate = document.getElementById("loan-rate");
+  formAmount.value = setInt.amount;
+  formYears.value = setInt.years;
+  formRate.value = setInt.rate;
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  formValues = getCurrentUIValues();
+  payment = calculateMonthlyPayment(formValues);
+  updateMonthly(payment);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  p = values.amount
+  i = (values.rate / 100) / 12;
+  n =  Math.floor(values.years * 12);
+  numerator = p * i;
+  denom = 1 - Math.pow((1 + i), -n)
+
+  return (
+    numerator / denom
+  ).toFixed(2);
+  
+
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+
+  solutionSpace = document.getElementById("monthly-payment");
+  solutionSpace.innerText = monthly;
 }
